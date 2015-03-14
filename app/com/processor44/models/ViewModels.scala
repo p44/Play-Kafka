5a0fcf34935cc0ebc646f12bac6bf32d7045f7ea
mwilson <mwilson@processor44.com>
2015-03-13 19:01:21 -0600

    View models

---
+++ app/com/processor44/models/ViewModels.scala
@@ -0,0 +1,17 @@
+package com.processor44.models
+
+import play.api.libs.json.Json
+
+case class Msg(msg: String)
+object Msg {
+  implicit val jsonWriter = Json.writes[Msg]
+  implicit val jsonReader = Json.reads[Msg]
+}
+
+/** */
+object ViewModels {
+  lazy val MSG_SUCCESS: Msg = Msg("Success")
+  lazy val MSG_SUCCESS_JSON = Json.prettyPrint(Json.toJson[Msg](MSG_SUCCESS))
+  lazy val MSG_BAD_JSON: Msg = Msg("Could not resolve json for Tick.  Check Content-Type and json format.")
+  lazy val MSG_BAD_JSON_JSON = Json.prettyPrint(Json.toJson[Msg](MSG_BAD_JSON))
+}
